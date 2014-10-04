a6c7f0431439530f0c8cbbdc4ea43610e3a0d562
zarra <xsp@oritsh.com>
2014-10-04 20:30:19 +0800

    ok

--- gdcm-codec/src/test/java/com/oritsh/imageIO/test/DecodecTest.java
+++ gdcm-codec/src/test/java/com/oritsh/imageIO/test/DecodecTest.java
@@ -38,11 +38,6 @@ public class DecodecTest {
 
         System.out.println("uncompress length:"+uncompress.length);
 
-        FileOutputStream outputStream = new FileOutputStream("uncompress.raw");
-        outputStream.write(uncompress);
-        outputStream.flush();
-        outputStream.close();
-
         decodec.dispose();
     }
 
@@ -70,10 +65,32 @@ public class DecodecTest {
 
         System.out.println("uncompress length:"+uncompress.length);
 
-        FileOutputStream outputStream = new FileOutputStream("uncompress.raw");
-        outputStream.write(uncompress);
-        outputStream.flush();
-        outputStream.close();
+        decodec.dispose();
+    }
+
+    @Test
+    public  void run3() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("jpeg2k.jp2");
+        int bufferSize = inputStream.available();
+        byte[] buffer = new byte[bufferSize];
+        inputStream.read(buffer);
+
+
+        Decodec decodec = GdcmCodecFactory.decodec(Decodec.GdcmDecodec.JPEG2KDECODEC);
+
+        boolean b = decodec.getHeaderInfo(buffer);
+
+        Assert.assertTrue(b);
+
+
+        System.out.println("width:"+decodec.getWidth());
+        System.out.println("height:"+decodec.getHeight());
+        System.out.println("BitsAllocated:"+decodec.getBitsAllocated());
+        System.out.println("SamplesPerPixel:"+decodec.getSamplesPerPixel());
+
+        byte[] uncompress = decodec.decode(buffer);
+
+        System.out.println("uncompress length:"+uncompress.length);
 
         decodec.dispose();
     }
